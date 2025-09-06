f2ccdf9ff1e9971cbeee3797b6470cef03b27e14
hemalekha <hemalekha4@gmail.com>
2025-09-06 15:17:19 +0530

    Deploy portfolio with HashRouter fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
@@ -10,23 +10,20 @@ import Experience from "./pages/Experience.jsx";
 import Skills from "./pages/Skills.jsx";
 import Contact from "./pages/Contact.jsx";
 
-
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
-      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about"element={<About/>}/>
-        <Route path="/projects"element={<Projects/>}/>
-        <Route path="/exper" element={<Experience/>}/>
-        <Route path="/skills" element={<Skills/>}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/exper" element={<Experience />} />
+        <Route path="/skills" element={<Skills />} />
         <Route path="/contact" element={<Contact />} />
-
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </HashRouter>
   );
 };
 
